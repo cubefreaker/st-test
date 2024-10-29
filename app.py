379b77ebfda9583a978108dd0050e9728e3bdf6c
Hamzah Habibi <44396732+cubefreaker@users.noreply.github.com>
2024-10-29 13:07:43 +0700

    Update app.py

--- app.py
+++ app.py
@@ -54,7 +54,7 @@ def show_chat_page():
         
         Jika pertanyaan tidak ada hubungannya dengan konteks data maka kamu akan menjawab: "Maaf, saya hanya bisa menjawab pertanyaan mengenai data penjualan invoice"
 
-        Kamu diberikan tabel dengan nama {os.environ.get('TABLE_SOURCE')} yang mempunyai kolom sebagai berikut:
+        Kamu diberikan tabel dengan nama `{os.environ.get('TABLE_SOURCE')}` yang mempunyai kolom sebagai berikut:
         - product (tipe atau nama produk)
         - invoice_no (nomor invoice)
         - inv_date (tanggal invoice dibuat)
@@ -66,15 +66,16 @@ def show_chat_page():
 
         Contoh:
         1.  Pertanyaan: Berapa jumlah total invoice keseluruhan?
-            Query: SELECT COUNT(*) FROM {os.environ.get('TABLE_SOURCE')}
+            Query: SELECT COUNT(*) FROM `{os.environ.get('TABLE_SOURCE')}`
         2.  Pertanyaan: Berapa Jumlah customer atau pelanggan dari semua invoice?
-            Query: SELECT COUNT(*) FROM {os.environ.get('TABLE_SOURCE')} GROUP BY customer_name
+            Query: SELECT COUNT(*) FROM `{os.environ.get('TABLE_SOURCE')}` GROUP BY customer_name
         3.  Pertanyaan: Berapa jumlah total penjualan pada bulan januari?
-            Query: SELECT SUM(sellprice) AS sellprice FROM {os.environ.get('TABLE_SOURCE')} WHERE inv_date BETWEEN '2024-01-01' AND '2024-01-31'
+            Query: SELECT SUM(sellprice) AS sellprice FROM `{os.environ.get('TABLE_SOURCE')}` WHERE inv_date BETWEEN '2024-01-01' AND '2024-01-31'
         4.  Pertanyaan: Berapa penjualan dari customer Yokogawa Indonesia, PT?
-            Query: SELECT SUM(sellprice) AS sellprice FROM {os.environ.get('TABLE_SOURCE')} WHERE customer_name = 'Yokogawa Indonesia, PT'
+            Query: SELECT SUM(sellprice) AS sellprice FROM `{os.environ.get('TABLE_SOURCE')}` WHERE customer_name = 'Yokogawa Indonesia, PT'
 
         Berikan hasil query sql tanpa tanda ``` dan juga tanpa penjelasan apapun, hanya kode sql nya saja!
+        Wrap nama tabel dengan tanda `, contohnya `nama-project.nama_table`
     """
 
     from_sql_prompt = f"""
@@ -82,19 +83,19 @@ def show_chat_page():
 
         Contoh:
         1.  Pertanyaan: Berapa jumlah total invoice keseluruhan?
-            Query: SELECT COUNT(*) FROM {os.environ.get('TABLE_SOURCE')}
+            Query: SELECT COUNT(*) FROM `{os.environ.get('TABLE_SOURCE')}`
             Hasil: [(1000,)]
             Jawaban: Jumlah total invoice keseluruhan adalah (Hasil) invoice
         2.  Pertanyaan: Berapa Jumlah customer atau pelanggan dari semua invoice?
-            Query: SELECT COUNT(*) FROM {os.environ.get('TABLE_SOURCE')} GROUP BY customer_name
+            Query: SELECT COUNT(*) FROM `{os.environ.get('TABLE_SOURCE')}` GROUP BY customer_name
             Hasil: [(1000,)]
             Jawaban: Jumlah total customer atau pelanggan yang terdapat pada invoice adalah (Hasil) customer
         3.  Pertanyaan: Berapa jumlah total penjualan pada bulan januari?
-            Query: SELECT SUM(sellprice) AS sellprice FROM {os.environ.get('TABLE_SOURCE')} WHERE inv_date BETWEEN '2024-01-01' AND '2024-01-31'
+            Query: SELECT SUM(sellprice) AS sellprice FROM `{os.environ.get('TABLE_SOURCE')}` WHERE inv_date BETWEEN '2024-01-01' AND '2024-01-31'
             Hasil: [(1000,)]
             Jawaban: Jumlah total penjualan pada bulan januari tahun [tahun] adalah Rp. (Hasil dalam format kurs)
         4.  Pertanyaan: Berapa penjualan dari customer Yokogawa Indonesia, PT?
-            Query: SELECT SUM(sellprice) AS sellprice FROM {os.environ.get('TABLE_SOURCE')} WHERE customer_name = 'Yokogawa Indonesia, PT'
+            Query: SELECT SUM(sellprice) AS sellprice FROM `{os.environ.get('TABLE_SOURCE')}` WHERE customer_name = 'Yokogawa Indonesia, PT'
             Hasil: [(1000,)]
             Jawaban: Jumlah total penjualan dari customer Yokogawa Indonesia, PT adalah Rp. (Hasil dalam format kurs)
         
