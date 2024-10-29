53a25fd434f0151f7e07cd0a4774081b181c20c6
Hamzah Habibi <44396732+cubefreaker@users.noreply.github.com>
2024-10-29 17:39:55 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,38 +1,18 @@
 import os, jwt
-import streamlit as st
-from google.cloud import bigquery
-import mysql.connector
 import vertexai
 from vertexai.generative_models import GenerativeModel
+from google.cloud import bigquery
+import streamlit as st
 
-PROJECT_ID = os.environ.get("GCP_PROJECT")
-LOCATION = os.environ.get("GCP_REGION")
-
-# def get_db_connection():
-#     conn = mysql.connector.connect(
-#         host=os.environ.get("QA_DB_HOST"),
-#         user=os.environ.get("QA_DB_USER"),
-#         password=os.environ.get("QA_DB_PASS"),
-#         database=os.environ.get("QA_DB_NAME"),
-#         port=os.environ.get("QA_DB_PORT")
-#     )
-#     return conn
+PROJECT_ID = os.getenv("GCP_PROJECT")
+LOCATION = os.getenv("GCP_REGION")
 
 def get_data_from_db(query):
     client = bigquery.Client()
     return client.query(query, project=PROJECT_ID).to_dataframe()
 
-# def get_data_from_db(query):
-#     conn = get_db_connection()
-#     cursor = conn.cursor()
-#     cursor.execute(query)
-#     rows = cursor.fetchall()
-#     cursor.close()
-#     conn.close()
-#     return rows
-
 def get_gemini_response(question, prompt):
-    model = GenerativeModel('gemini-pro')
+    model = GenerativeModel('gemini-1.5-pro')
     response = model.generate_content([prompt, question])
     return response.text
 
@@ -41,7 +21,7 @@ def auth_data():
     if 't' in st.query_params:
         token = st.query_params.t
         try:    
-            data = jwt.decode(token, os.environ.get("JWT_KEY"), algorithms=["HS256"])
+            data = jwt.decode(token, os.getenv("JWT_KEY"), algorithms=["HS256"])
         except:
             pass
     return data
@@ -54,7 +34,7 @@ def show_chat_page():
         
         Jika pertanyaan tidak ada hubungannya dengan konteks data maka kamu akan menjawab: "Maaf, saya hanya bisa menjawab pertanyaan mengenai data penjualan invoice"
 
-        Kamu diberikan tabel dengan nama `{os.environ.get('TABLE_SOURCE')}` yang mempunyai kolom sebagai berikut:
+        Kamu diberikan tabel dengan nama `{os.getenv('TABLE_SOURCE')}` yang mempunyai kolom sebagai berikut:
         - product (tipe atau nama produk)
         - invoice_no (nomor invoice)
         - inv_date (tanggal invoice dibuat)
@@ -66,13 +46,13 @@ def show_chat_page():
 
         Contoh:
         1.  Pertanyaan: Berapa jumlah total invoice keseluruhan?
-            Query: SELECT COUNT(*) FROM `{os.environ.get('TABLE_SOURCE')}`
+            Query: SELECT COUNT(*) FROM `{os.getenv('TABLE_SOURCE')}`
         2.  Pertanyaan: Berapa Jumlah customer atau pelanggan dari semua invoice?
-            Query: SELECT COUNT(*) FROM `{os.environ.get('TABLE_SOURCE')}` GROUP BY customer_name
+            Query: SELECT COUNT(*) FROM `{os.getenv('TABLE_SOURCE')}` GROUP BY customer_name
         3.  Pertanyaan: Berapa jumlah total penjualan pada bulan januari?
-            Query: SELECT SUM(sellprice) AS sellprice FROM `{os.environ.get('TABLE_SOURCE')}` WHERE inv_date BETWEEN '2024-01-01' AND '2024-01-31'
+            Query: SELECT SUM(sellprice) AS sellprice FROM `{os.getenv('TABLE_SOURCE')}` WHERE inv_date BETWEEN '2024-01-01' AND '2024-01-31'
         4.  Pertanyaan: Berapa penjualan dari customer Yokogawa Indonesia, PT?
-            Query: SELECT SUM(sellprice) AS sellprice FROM `{os.environ.get('TABLE_SOURCE')}` WHERE customer_name = 'Yokogawa Indonesia, PT'
+            Query: SELECT SUM(sellprice) AS sellprice FROM `{os.getenv('TABLE_SOURCE')}` WHERE customer_name = 'Yokogawa Indonesia, PT'
 
         Berikan hasil query sql tanpa tanda ``` dan juga tanpa penjelasan apapun, hanya kode sql nya saja!
         Wrap nama tabel dengan tanda `, contohnya `nama-project.nama_table`
@@ -83,19 +63,19 @@ def show_chat_page():
 
         Contoh:
         1.  Pertanyaan: Berapa jumlah total invoice keseluruhan?
-            Query: SELECT COUNT(*) FROM `{os.environ.get('TABLE_SOURCE')}`
+            Query: SELECT COUNT(*) FROM `{os.getenv('TABLE_SOURCE')}`
             Hasil: [(1000,)]
             Jawaban: Jumlah total invoice keseluruhan adalah (Hasil) invoice
         2.  Pertanyaan: Berapa Jumlah customer atau pelanggan dari semua invoice?
-            Query: SELECT COUNT(*) FROM `{os.environ.get('TABLE_SOURCE')}` GROUP BY customer_name
+            Query: SELECT COUNT(*) FROM `{os.getenv('TABLE_SOURCE')}` GROUP BY customer_name
             Hasil: [(1000,)]
             Jawaban: Jumlah total customer atau pelanggan yang terdapat pada invoice adalah (Hasil) customer
         3.  Pertanyaan: Berapa jumlah total penjualan pada bulan januari?
-            Query: SELECT SUM(sellprice) AS sellprice FROM `{os.environ.get('TABLE_SOURCE')}` WHERE inv_date BETWEEN '2024-01-01' AND '2024-01-31'
+            Query: SELECT SUM(sellprice) AS sellprice FROM `{os.getenv('TABLE_SOURCE')}` WHERE inv_date BETWEEN '2024-01-01' AND '2024-01-31'
             Hasil: [(1000,)]
             Jawaban: Jumlah total penjualan pada bulan januari tahun [tahun] adalah Rp. (Hasil dalam format kurs)
         4.  Pertanyaan: Berapa penjualan dari customer Yokogawa Indonesia, PT?
-            Query: SELECT SUM(sellprice) AS sellprice FROM `{os.environ.get('TABLE_SOURCE')}` WHERE customer_name = 'Yokogawa Indonesia, PT'
+            Query: SELECT SUM(sellprice) AS sellprice FROM `{os.getenv('TABLE_SOURCE')}` WHERE customer_name = 'Yokogawa Indonesia, PT'
             Hasil: [(1000,)]
             Jawaban: Jumlah total penjualan dari customer Yokogawa Indonesia, PT adalah Rp. (Hasil dalam format kurs)
         
@@ -124,20 +104,21 @@ def show_chat_page():
             with st.status("Mencari jawaban"):
                 # question = input('Question: ')
                 resp_query = get_gemini_response(question, to_sql_prompt)
+                print(resp_query)
                 resp_query = resp_query.replace('```sql', '').replace('```', '').strip()
                 # print('Query: ', resp_query, '\n')
-                try:
-                    result_query = get_data_from_db(resp_query)
-                    final_question = f"""
-                        Pertanyaan: {question}
-                        Query: {resp_query}
-                        Hasil: {str(result_query)}
-                    """
-                    # print(final_question, '\n')
+                # try:
+                result_query = get_data_from_db(resp_query)
+                final_question = f"""
+                    Pertanyaan: {question}
+                    Query: {resp_query}
+                    Hasil: {str(result_query)}
+                """
+                # print(final_question, '\n')
 
-                    resp_final = get_gemini_response(final_question, from_sql_prompt)
-                except:
-                    resp_final = resp_query
+                resp_final = get_gemini_response(final_question, from_sql_prompt)
+                # except:
+                #     resp_final = resp_query
 
                 # print('Final Result: ', resp_final, '\n')
             st.markdown(resp_final)
